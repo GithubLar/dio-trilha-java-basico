019596cb832652c63ce8e08baca4e7bc9a5a6ccc
GithubLar <larissa_menesso@hotmail.com>
2023-01-29 19:53:59 -0300

    message

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,17 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+
+        boolean condicao1 = false;
+        boolean condicao2 = true;
+
+        if(condicao1 && condicao2){
+            System.out.println("as condicoes");
+        }
+        if(condicao1 || condicao2){
+            System.out.println("uma");
+        }
+    
+    }
+
+}
